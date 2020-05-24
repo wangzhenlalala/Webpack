d46be171bbac8ae4676260ea1df45161506cb7aa
christopher wang <theflyingwhite@163.com>
2020-05-24 18:12:38 +0800

    manifest,split chunk

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const util = require('util');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Outpue Management',
+        }),
+        new ManifestPlugin({
+            generate(seed, files, entryPoints) {
+                // console.log(util.inspect(files, false, null, true /* enable colors */))
+                fs.writeFileSync( path.resolve( process.cwd(), 'dist/__manifest__webpack.js'), util.inspect(files));
+                // util.inspect(entryPoints);
+                // default behavior
+                return files.reduce( (manifest, file) => {
+                    manifest[file.name] = file.path;
+                    return manifest;
+                }, seed);
+            }
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        }
+    }
+}
