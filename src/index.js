97019a7e86a5bb8f0a4edcc17c26a4638cc38037
wangzhen <thewhitelelphant@163.com>
2020-05-31 14:10:24 +0800

    webpack 会自己对动态加载的module命名，我的webpackChunkName错了 ^_^

--- src/index.js
+++ src/index.js
@@ -1,42 +1,22 @@
 import "./style.css";
-import common_1 from './common-1';
 
-function component(textContent) {
-    const $div = $('<div></div>');
-    $div.addClass('hello')
-        .text(textContent);
-
-
-    const $btn = $('<button></button>')
-        .text('print')
-        .off('click')
-        .on('click', (e) => {
-            e.target.textContent = "loading...";
-            import(/** webpackChunkName common-1*/ './common-1.js').then( module => {
-                setTimeout(() => {
-                    e.target.textContent = "loaded";
-                }, 1500);
-            })
-        });
-    
-    $div.append($btn);
-    
-    return $div;
-}
-
-function getComponent(text) {
-    return import(/* webpackChunkName: "dynamic-load-jquery" */ 'jquery').then( ({default: $}) => {
-        // 动态的加载来jquery之后，全局上并没有JQuery,$的实例； 仅仅在这里，我们能够引用jquery模块
-        console.log($);
-        return component(text);
-    })
+function renderBtn(textContent) {
+    const divElement = document.createElement('div');
+    const btnElement = document.createElement('button');
+    btnElement.textContent = "to load";
+    btnElement.classList.add('btn');
+    btnElement.addEventListener('click',e => {
+        e.target.textContent = "loading...";
+        import(/** webpackChunkName common-1*/ './common-1.js').then( module => {
+            setTimeout(() => {
+                e.target.textContent = "loaded";
+            }, 1500);
+            console.log(module);
+        })
+    });
+    document.body.appendChild(btnElement)
 }
 
 window.addEventListener('load', (e) => {
-    // getComponent().then( com => {
-    //     $('body').append(com);
-    //     // console.log($); // 这里并不能引用jquery, // ReferenceError: $ is not defined
-    // });
-    document.body.appendChild( component("hello").get(0));
-
+    renderBtn();
 });
